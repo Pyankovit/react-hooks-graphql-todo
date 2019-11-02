c982ff95e7ed09970ae3ddd4ede55116ce7d422e
pyankovit <pyankov.it@gmail.com>
2019-11-02 17:48:59 +0200

    new commit added gata from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, useQuery, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const GET_TODOS = gql`query {  
+  todos {
+    id,
+    title,
+    completed
+  }
+}`;
+
+const client = new ApolloClient({
+  uri: "https://todo-mongo-graphql-server.herokuapp.com"
+});
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+      console.log(todo, index)
+      return (
+        <div
+          className="todo"
+          style={{ textDecoration: todo.completed ? "line-through" : "" }}
+        >
+          {todo.title}
+          <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+            <button onClick={() => removeTodo(index)}>x</button>
+          </div>
+        </div>
+      );
+    }
 
 function App() {
+  
+  const { loading, error, data } = useQuery(GET_TODOS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const addTodo = title => {
+    const newTodos = [...data, { title, completed: false }];
+    console.log(newTodos);
+    console.log(data);
+    //setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...data];
+    newTodos[index].completed = true;
+    //setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...data];
+    newTodos.splice(index, 1);
+    //setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        {data.todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+//import App from './App.js'
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://todo-mongo-graphql-server.herokuapp.com"
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const GET_TODOS = gql`query {  
+  todos {
+    id,
+    title,
+    completed
+  }
+}`;
+
+const ADD_TODO = gql`mutation{
+  add (title: "Testing graphql") {
+    id,
+    title,
+    completed
+  }
+}`;
+
+function Todo(completeTodo) {
+  const { loading, error, data } = useQuery(GET_TODOS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return data.todos.map((todo, index) => (
+    <div key={index}>
+    <div
+      className="todo"
+      style={{ textDecoration: todo.completed ? "line-through" : "" }}
+    >
+      {todo.title}
+            <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button>x</button>
+      </div>
+      </div>
+    </div>
+  ));
+}
+
+function App() {
+
+  // const addTodo = title => {
+  //   const newTodos = [...data, { title, completed: false }];
+  //   console.log(newTodos);
+  //   console.log(data);
+  //   //setTodos(newTodos);
+  // };
+
+  const completeTodo = index => {
+  //   const newTodos = [...data];
+  //   newTodos[index].completed = true;
+  //   //setTodos(newTodos);
+  console.log(index);
+  };
+
+  // const removeTodo = index => {
+  //   const newTodos = [...data];
+  //   newTodos.splice(index, 1);
+  //   //setTodos(newTodos);
+  // };
+  
+  return (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <Todo
+        
+      />
+    </div>
+  </ApolloProvider>
+  )}
+
+render(<App />, document.getElementById("root"));
